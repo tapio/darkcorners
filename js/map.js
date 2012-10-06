157b4403e0026c25b9a4044cb4e50807cfb7be28
Tapio Vierros <tapiovierros@gmail.com>
2012-10-06 16:53:10 +0300

    Oops, added critical forgotten file.

---
+++ js/map.js
@@ -0,0 +1,37 @@
+var OPEN = ".";
+var WALL = "#";
+
+function Map(w, h, data) {
+	this.map = new Array(w * h);
+
+	if (data && data.length && data instanceof Array) {
+		for (var j = 0; j < h; ++j) {
+			for (var i = 0; i < w; ++i) {
+				this.map[j * w + i] = data[j][i];
+			}
+		}
+	} else if (data) {
+		for (var k = 0; k < w * h; ++k) this.map[k] = data;
+	}
+
+	this.get = function(x, y, fallback) {
+		if (x < 0 || x >= w || y < 0 || y >= h) return fallback || null;
+		return this.map[y * w + x];
+	};
+
+	this.put = function(x, y, what) {
+		if (x < 0 || x >= w || y < 0 || y >= h) return;
+		this.map[y * w + x] = what;
+	};
+
+	this.toJSON = function() {
+		var res = new Array(h);
+		for (var j = 0; j < h; ++j) {
+			res[j] = "";
+			for (var i = 0; i < w; ++i) {
+				res[j] += this.map[j * w + i];
+			}
+		}
+		return res;
+	};
+}
