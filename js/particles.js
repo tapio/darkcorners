8284b74672651164a62642ef88c1965c23832c8d
Tapio Vierros <tapiovierros@gmail.com>
2012-10-07 18:53:29 +0300

    Whitespace.

--- js/particles.js
+++ js/particles.js
@@ -53,61 +53,61 @@ function createTexturedFire(parent) {
 	var numSprites = 8;
 	var emitter = Fireworks.createEmitter({ nParticles: 20 });
 	emitter.effectsStackBuilder()
-			.spawnerSteadyRate(15)
-			.position(Fireworks.createShapeSphere(0, 0.05, 0, 0.05))
-			.velocity(Fireworks.createShapePoint(0, 1.5, 0))
-			.lifeTime(0.1, 0.3)
-			.friction(0.99)
-			//.randomVelocityDrift(Fireworks.createVector(0.1,2,0))
-			.createEffect('scale', {
-					origin: 1/1000,
-					factor: 1.02
-				}).onBirth(function(particle) {
-					var object3d = particle.get('threejsObject3D').object3d;
-					var scale = this.opts.origin;
-					object3d.scale.set(scale*1.5, scale*4)
-				}).onUpdate(function(particle, deltaTime) {
-					var object3d = particle.get('threejsObject3D').object3d;
-					object3d.scale.multiplyScalar(this.opts.factor);
-				}).back()
-			.createEffect('rotation')
-				.onBirth(function(particle) {
-					var object3d = particle.get('threejsObject3D').object3d;
-					object3d.rotation = Math.random() * Math.PI * 2;
-				}).back()
-			.createEffect('opacity', {
-					gradient: Fireworks.createLinearGradient()
-							.push(0.00, 0.00)
-							.push(0.05, 1.00)
-							.push(0.99, 1.00)
-							.push(1.00, 0.00)
-				}).onUpdate(function(particle) {
-					var object3d = particle.get('threejsObject3D').object3d;
-					var canonAge = particle.get('lifeTime').normalizedAge();
-					object3d.opacity = this.opts.gradient.get(canonAge);
-				}).back()
-			.renderToThreejsObject3D({
-				container: parent,
-				create: function() {
-					var object3d = new THREE.Sprite({
-						useScreenCoordinates: false,
-						map: texture,
-						blending: THREE.AdditiveBlending,
-						transparent: true
-					});
-					object3d.uvScale.set(1, 1 / numSprites)
-					return object3d;
-				}
-			})
-			.createEffect("updateSpritesheet")
-				.onUpdate(function(particle, deltaTime) {
-					var object3d = particle.get('threejsObject3D').object3d;
-					var canonAge = particle.get('lifeTime').normalizedAge();
-					var imageIdx = Math.floor(canonAge * (numSprites));
-					var uvOffsetY = imageIdx * 1 / numSprites;
-					object3d.uvOffset.set(0, uvOffsetY)
-				}).back()
-			.back()
-		.start();
+		.spawnerSteadyRate(15)
+		.position(Fireworks.createShapeSphere(0, 0.05, 0, 0.05))
+		.velocity(Fireworks.createShapePoint(0, 1.5, 0))
+		.lifeTime(0.1, 0.3)
+		.friction(0.99)
+		//.randomVelocityDrift(Fireworks.createVector(0.1,2,0))
+		.createEffect('scale', {
+				origin: 1/1000,
+				factor: 1.02
+			}).onBirth(function(particle) {
+				var object3d = particle.get('threejsObject3D').object3d;
+				var scale = this.opts.origin;
+				object3d.scale.set(scale*1.5, scale*4)
+			}).onUpdate(function(particle, deltaTime) {
+				var object3d = particle.get('threejsObject3D').object3d;
+				object3d.scale.multiplyScalar(this.opts.factor);
+			}).back()
+		.createEffect('rotation')
+			.onBirth(function(particle) {
+				var object3d = particle.get('threejsObject3D').object3d;
+				object3d.rotation = Math.random() * Math.PI * 2;
+			}).back()
+		.createEffect('opacity', {
+				gradient: Fireworks.createLinearGradient()
+						.push(0.00, 0.00)
+						.push(0.05, 1.00)
+						.push(0.99, 1.00)
+						.push(1.00, 0.00)
+			}).onUpdate(function(particle) {
+				var object3d = particle.get('threejsObject3D').object3d;
+				var canonAge = particle.get('lifeTime').normalizedAge();
+				object3d.opacity = this.opts.gradient.get(canonAge);
+			}).back()
+		.renderToThreejsObject3D({
+			container: parent,
+			create: function() {
+				var object3d = new THREE.Sprite({
+					useScreenCoordinates: false,
+					map: texture,
+					blending: THREE.AdditiveBlending,
+					transparent: true
+				});
+				object3d.uvScale.set(1, 1 / numSprites)
+				return object3d;
+			}
+		})
+		.createEffect("updateSpritesheet")
+			.onUpdate(function(particle, deltaTime) {
+				var object3d = particle.get('threejsObject3D').object3d;
+				var canonAge = particle.get('lifeTime').normalizedAge();
+				var imageIdx = Math.floor(canonAge * (numSprites));
+				var uvOffsetY = imageIdx * 1 / numSprites;
+				object3d.uvOffset.set(0, uvOffsetY)
+			}).back()
+		.back()
+	.start();
 	return emitter;
 }
