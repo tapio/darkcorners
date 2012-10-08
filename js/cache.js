69bcdb4b444b4174389d319be7c9bbec8c94c565
Tapio Vierros <tapiovierros@gmail.com>
2012-10-08 22:25:59 +0300

    Display loading message when models are pending.

--- js/cache.js
+++ js/cache.js
@@ -4,17 +4,30 @@ function Cache() {
 	this.geometries = {};
 	this.materials = {};
 	var self = this;
-	var loader = new THREE.JSONLoader();
+	var loader = new THREE.JSONLoader(true);
+	loader.statusDomElement.style.left = "0px";
+	loader.statusDomElement.style.fontSize = "1.8em";
+	loader.statusDomElement.style.width = "auto";
+	loader.statusDomElement.style.color = "#c00";
+	document.body.appendChild(loader.statusDomElement);
+	var modelsPending = 0;
 
 	this.loadModel = function(path, callback) {
 		var m = this.models[path];
 		if (!m) { // First time request for this model
 			this.models[path] = [ callback ];
+			loader.statusDomElement.style.display = "block";
+			loader.statusDomElement.innerHTML = "Loading " + modelsPending + " models...";
+			modelsPending++;
 			loader.load(path, function(geometry) {
 				var mm = self.models[path];
 				for (var i = 0; i < mm.length; ++i)
 					mm[i](geometry);
 				self.models[path] = geometry;
+				modelsPending--;
+				if (modelsPending == 0)
+					loader.statusDomElement.style.display = "none";
+				else loader.statusDomElement.innerHTML = "Loading " + modelsPending + " models...";
 			});
 		} else if (m instanceof Array) { // Pending
 			m.push(callback);
