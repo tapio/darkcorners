fa773b99aa377122bba18d0a79c7322f2fcb4465
Tapio Vierros <tapiovierros@gmail.com>
2012-12-03 19:51:32 +0200

    Skip LightManager updates if there isn't enough lights/shadows.

--- js/lights.js
+++ js/lights.js
@@ -17,37 +17,42 @@ function LightManager(params) {
 	var v1 = new THREE.Vector2();
 	var v2 = new THREE.Vector2();
 
+	function distSq(a, b) {
+		var dx = b.x - a.x, dz = b.z - a.z;
+		return dx * dx + dz * dz;
+	}
+
 	this.update = function(observer) {
 		function angleDist(a, b) {
 			v1.set(a.x - observer.position.x, a.z - observer.position.z).normalize();
 			v2.set(b.x - observer.position.x, b.z - observer.position.z).normalize();
 			return Math.acos(v1.dot(v2));
 		}
-		function distSq(a, b) {
-			var dx = b.x - a.x, dz = b.z - a.z;
-			return dx * dx + dz * dz;
-		}
 		function sortByDist(a, b) {
 			return distSq(a.position, observer.position) - distSq(b.position, observer.position);
 		}
+
 		var i, used = 0;
 
-		this.lights.sort(sortByDist);
-		for (i = 0; i < this.lights.length; ++i) {
-			if (used < this.maxLights && (
-				angleDist(this.lights[i].position, controls.target) < Math.PI * 0.4 ||
-				distSq(this.lights[i].position, observer.position) < 1.5 * this.lights[i].distance * this.lights[i].distance))
-				{
-					this.lights[i].visible = true;
-					++used;
-			} else this.lights[i].visible = false;
+		if (this.maxLights < this.lights.length) {
+			this.lights.sort(sortByDist);
+			for (i = 0; i < this.lights.length; ++i) {
+				if (used < this.maxLights && (
+					angleDist(this.lights[i].position, controls.target) < Math.PI * 0.4 ||
+					distSq(this.lights[i].position, observer.position) < 1.5 * this.lights[i].distance * this.lights[i].distance))
+					{
+						this.lights[i].visible = true;
+						++used;
+				} else this.lights[i].visible = false;
+			}
 		}
 
-		this.shadows.sort(sortByDist);
-		for (i = 0; i < this.shadows.length; ++i) {
-			if (i < this.maxShadows) this.shadows[i].castShadow = true;
-			else this.shadows[i].castShadow = false;
+		if (this.maxShadows < this.shadows.length) {
+			this.shadows.sort(sortByDist);
+			for (i = 0; i < this.shadows.length; ++i) {
+				if (i < this.maxShadows) this.shadows[i].castShadow = true;
+				else this.shadows[i].castShadow = false;
+			}
 		}
-
 	};
 }
