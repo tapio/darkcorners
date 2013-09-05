59aecd8a51831546aa7dd0f68c0b41b3150f73c3
Tapio Vierros <tapiovierros@gmail.com>
2013-09-05 22:38:58 +0300

    Firefox now supports fullscreenless pointer lock.

--- js/ui.js
+++ js/ui.js
@@ -32,8 +32,7 @@ DC.initUI = function() {
 	$(window).blur(pause);
 	$(window).focus(resume);
 	$("#instructions").click(function() {
-		// Firefox doesn't support fullscreenless pointer lock, so resort to this hack
-		if (CONFIG.fullscreen || /Firefox/i.test(navigator.userAgent)) {
+		if (CONFIG.fullscreen) {
 			var onFullscreenChange = function(event) {
 				if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullscreenElement || document.mozFullScreenElement) {
 					document.removeEventListener('fullscreenchange', onFullscreenChange);
